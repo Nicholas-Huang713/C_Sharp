73c892c36d096450c1cf944b7f376425b86e2acd
Nicholas_Huang713 <nhuang713@gmail.com>
2019-08-10 19:56:42 -0700

    Dojo Survey Completed

---
+++ DojoSurvey/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("submit")]
+        public IActionResult Submit(string name, string location, string language, string comment)
+        {
+            return RedirectToAction("Show", new {name = name, location = location, language = language, comment = comment});
+        }
+
+        [HttpGet("show/{name}/{location}/{language}/{comment}")]
+        public ViewResult Show(string name, string location, string language, string comment)
+        {
+            ViewBag.Name = name;
+            ViewBag.Location = location;
+            ViewBag.Language = language;
+            ViewBag.Comment = comment;
+            return View("Show");
+        }
+    }
+}
