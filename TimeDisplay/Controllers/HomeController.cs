2f7e732beed3efd7abd16810401d34f2f661008b
Nicholas_Huang713 <nhuang713@gmail.com>
2019-08-10 18:55:04 -0700

    Time Display added

---
+++ TimeDisplay/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TimeDisplay.Models;
+
+namespace TimeDisplay.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            DateTime CurrentTime = DateTime.Now;
+            ViewBag.Date = CurrentTime.ToString("MMMM dd, yyyy");
+            ViewBag.Time = CurrentTime.ToString("h:mm tt");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
